2e9a4ebf2ebb0e53271d9c4add241bb46e2d8a3c
AriiqAlhafizh <ariiqalhafizh69@gmail.com>
2025-12-01 12:35:49 +0700

    Add enemy prefabs
    add Enemy Prefabs that can chase player

---
+++ Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int health = 100;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
